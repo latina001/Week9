16b2ae8b54aaf866ed85a3cbc7ade2a5cc760109
latina001 <nannaphat20160@gmail.com>
2026-03-10 10:34:58 +0700

    Set control air  in game and Script

--- Assets/Script/Airplane.cs
+++ Assets/Script/Airplane.cs
@@ -8,6 +8,10 @@ public class Airplane : MonoBehaviour
     public float drang = 0.001f;
     public float angularDrag = 0.001f;
 
+    public float yawPower = 50f;//turnSpeed
+    public float pitchPower = 50f;
+    public float rollPower = 30f;
+
     private Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,5 +34,23 @@ public class Airplane : MonoBehaviour
             rb.linearDamping = rb.linearVelocity.magnitude * drang;
             rb.linearDamping = rb.linearVelocity.magnitude * angularDrag;
         }
+    }
+    private void Update()
+    {
+        var keyboard = Keyboard.current;
+        if (keyboard == null) return;
+
+        float yaw = (Keyboard.current.eKey.isPressed ? 1f : 0f) - (Keyboard.current.qKey.isPressed ? 1f : 0f);
+        yaw *= yawPower;
+
+        float pitch = (Keyboard.current.sKey.isPressed ? 1f : 0f) - (Keyboard.current.wKey.isPressed ? 1f : 0f);
+        pitch *= pitchPower;
+
+        float roll = (Keyboard.current.aKey.isPressed ? 1f : 0f) - (Keyboard.current.dKey.isPressed ? 1f : 0f);
+        roll *= rollPower;
+
+        rb.AddTorque(transform.up * yaw);
+        rb.AddTorque(transform.right * pitch);
+        rb.AddTorque(transform.forward * roll);
     }
 }
